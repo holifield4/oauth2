6ea75a17c2c6f29874809632e10466950dea0773
holifieldw <holifield04@gmail.com>
2025-10-22 13:56:59 +0800

    api calls and modify css styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function navigate(url: string) {
+    window.location.href = url;
+  }
+
+  async function auth() {
+    try {
+      const response = await fetch(
+        "http://localhost:3000/api/v1/auth/authorize",
+        {
+          method: "POST",
+        }
+      );
+
+      const data = await response.json();
+      navigate(data.url);
+    } catch (error) {
+      console.error("Auth error:", error);
+    }
+  }
+
+  async function getGreetings() {
+    try {
+      const response = await fetch("http://localhost:3000/api/v1/hello", {
+        method: "GET",
+        credentials: "include",
+      }).then((res) => res.json());
+
+      if (response.error) {
+        alert(response.error);
+      } else {
+        alert(response.message);
+      }
+    } catch (error) {
+      alert("Alert: " + error);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="main-container">
+      <h1>OAuth2</h1>
+      <div className="button-container">
+        <button type="button" onClick={() => auth()}>
+          Sign in with Google
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button type="button" onClick={() => getGreetings()}>
+          Greetings from server
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
