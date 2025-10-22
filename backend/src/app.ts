bab7972c55d115d10864a324e9055144ba68b716
holifieldw <holifield04@gmail.com>
2025-10-22 13:57:45 +0800

    enable cors and cookie parser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,20 @@
 import express from 'express';
 import authRoutes from "./routes/auth/auth.route";
+import greetingRoutes from "./routes/protected/greetings.route";
 import { errorHandler } from './middleware/errorHandler';
+import cors from "cors";
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(cookieParser());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+}))
 
 app.use(express.json());
 
@@ -10,6 +22,7 @@ const baseUri = "/api/v1";
 
 // Routes
 app.use(baseUri, authRoutes);
+app.use(baseUri, greetingRoutes);
 
 app.use(errorHandler);
 
